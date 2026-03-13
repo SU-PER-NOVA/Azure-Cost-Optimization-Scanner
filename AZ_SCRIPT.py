340fd22c68919772c0ae956e8986266d542a6efc
Karan Tolamatti <118611454+SU-PER-NOVA@users.noreply.github.com>
2026-03-13 14:45:26 +0530

    Restore docstring and main function call

--- AZ_SCRIPT.py
+++ AZ_SCRIPT.py
@@ -13,7 +13,7 @@ import datetime
 
 # Authentication
 credential = DefaultAzureCredential()
-subscription_id = "ADD YOUR SUBSCRIPTION ID"
+subscription_id = "ADD YOUR SUBSCRIPTION ID" #RUN az login and copy subscription id for your azure account
 
 compute_client = ComputeManagementClient(credential, subscription_id)
 network_client = NetworkManagementClient(credential, subscription_id)
@@ -126,4 +126,5 @@ def main():
     print_report(vms, disks, ips, nics)
 
 if __name__ == "__main__":
+
     main()
